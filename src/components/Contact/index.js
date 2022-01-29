815f531c12ae5b834588c88c54310e39a9c3221c
paperpatch <patchen21@gmail.com>
2022-01-29 18:14:56 -0500

    validate form data

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,30 @@
 import React, { useState } from "react";
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  const [errorMessage, setErrorMessage] = useState('');
   const { name, email, message } = formState;
 
   function handleChange(e) {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        if (!e.target.value.length) {
+          setErrorMessage(`${e.target.name} is required.`)
+        } else {
+          setErrorMessage('');
+        }
+      }
+    }
     setFormState({...formState, [e.target.name]: e.target.value })
+
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
   }
 
   function handleSubmit(e) {
